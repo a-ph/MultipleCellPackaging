5addb859738aaff81cfa635940e474742755ac6e
yifei.xing <yifei.xing@ushow.media>
2018-08-30 17:19:20 +0800

    pod commit

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'starmaker8.0' do
+  # Uncomment this line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+
+  # Pods for starmaker8.0
+
+  pod 'Masonry', '1.1.0'
+  target 'starmaker8.0Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'starmaker8.0UITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- starmaker8.0/SMHomeCellModel.h
+++ starmaker8.0/SMHomeCellModel.h
@@ -8,6 +8,28 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ 卡片类型
+ */
+typedef NS_ENUM(NSUInteger, SMHomeTableViewCellCardType) {
+    SMHomeTableViewCellCardTypeSingAudio,//音频作品
+    SMHomeTableViewCellCardTypeSingVideo,//视频作品
+    SMHomeTableViewCellCardTypeImage,//图片
+    SMHomeTableViewCellCardTypeText,//文字
+    SMHomeTableViewCellCardTypeUserVideo,//用户上传的视频
+};
+
+/**
+ 转发\原博,类型
+ */
+typedef NS_ENUM(NSUInteger, SMHomeTableViewCellContentType) {
+    SMHomeTableViewCellTypeContentOriginal,//原博
+    SMHomeTableViewCellTypeContentForward,//转发
+};
+
 @interface SMHomeCellModel : NSObject
+
+@property (nonatomic, copy) NSString *identifier;
+@property (nonatomic, assign) NSInteger height;
 
 @end
